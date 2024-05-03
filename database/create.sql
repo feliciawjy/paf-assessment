c79a8fa82b9f3635425df84bf62b3a638d2a248d
feliciawjy <feliciawjy@gmail.com>
2024-05-03 09:56:56 +0800

    configured application properties

---
+++ database/create.sql
@@ -0,0 +1,8 @@
+-- Task 1
+-- Write SQL statements in this file to create the brewery database and 
+-- populate the database with the given SQL files
+
+CREATE SCHEMA brewery;
+
+source 
+
--- src/main/java/ibf2024/assessment/paf/batch4/Batch2Application.java
+++ src/main/java/ibf2024/assessment/paf/batch4/Batch2Application.java
@@ -1,13 +1,20 @@
 package ibf2024.assessment.paf.batch4;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Batch2Application {
+public class Batch2Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Batch2Application.class, args);
 	}
+
+	// @Override
+	// public void run(String... args) throws Exception {
+	// 	// TODO Auto-generated method stub
+	// 	throw new UnsupportedOperationException("Unimplemented method 'run'");
+	// }
 
 }
